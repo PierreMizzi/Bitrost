b57962ee602da0b0ba1e52441c4276c5e126b8b7
Pierre Mizzi <pierre.mizza@gmail.com>
2023-07-28 06:38:12 +0900

    Panning camera behaviour

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -13,7 +13,7 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private float m_distance = 4f;
 
-        [SerializeField]
+    [SerializeField]
     private InputActionReference m_mousePositionActionReference = null;
 
     private Vector3 m_mousePositionValue;
@@ -22,19 +22,26 @@ public class CameraController : MonoBehaviour
 
     private Vector3 mousePositionScreenCenter
     {
-        get {
-            return new Vector3(m_mousePositionValue.x - m_camera.pixelWidth / 2f, m_mousePositionValue.y - m_camera.pixelHeight /2f);
+        get
+        {
+            return new Vector3(
+                m_mousePositionValue.x - m_camera.pixelWidth / 2f,
+                m_mousePositionValue.y - m_camera.pixelHeight / 2f
+            );
         }
     }
 
-    [SerializeField] private Vector2 m_debugCenterScreen;
+    [SerializeField]
+    private float m_treshold = 0.75f;
 
-    private float m_distanceFromCenter;
+    [SerializeField]
+    private float m_offsetMagnitude = 1f;
 
-    [SerializeField] private float m_minRadius = 1f;
-    [SerializeField] private float m_maxRadius = 2f;
+    [SerializeField]
+    private float m_smoothDampSpeed;
 
-    [SerializeField] private float m_offsetMagnitude = 1f;
+    [SerializeField]
+    private float m_smoothDampMaxSpeed;
 
     private float m_offset;
 
@@ -44,14 +51,14 @@ public class CameraController : MonoBehaviour
 
     #region Methods
 
-    private void Awake() {
+    private void Awake()
+    {
         m_camera = Camera.main;
     }
 
     private void Update()
     {
         m_mousePositionValue = m_mousePositionActionReference.action.ReadValue<Vector2>();
-        m_debugCenterScreen = mousePositionScreenCenter;
     }
 
     private void LateUpdate()
@@ -60,20 +67,56 @@ public class CameraController : MonoBehaviour
         // transform.position = new Vector3(m_target.position.x, m_target.position.y, -m_distance);
     }
 
+    private Vector3 velocity;
+
     private void ManageCameraPosition()
     {
-        m_distanceFromCenter = (Vector3.zero - mousePositionScreenCenter).magnitude;
-        m_offset = UtilsClass.Remap(m_distanceFromCenter, m_minRadius, m_maxRadius, 0f, 1f);
+        float angle = Mathf.Atan2(mousePositionScreenCenter.y, mousePositionScreenCenter.x);
+
+        float edgeMagnitude = MagnitudeFromAngle(angle);
+
+        float thresholdEdgeMagnitude = edgeMagnitude * m_treshold;
+
+        float mousePositionMangitude = mousePositionScreenCenter.magnitude;
+
+        m_offset = UtilsClass.Remap(
+            mousePositionMangitude,
+            thresholdEdgeMagnitude,
+            edgeMagnitude,
+            0f,
+            1f
+        );
         m_offset = Mathf.Clamp01(m_offset);
 
         m_cameraPosition = m_target.position;
         m_cameraPosition.z = -1;
 
+        m_cameraPosition += mousePositionScreenCenter.normalized * m_offset * m_offsetMagnitude;
 
-        m_cameraPosition += mousePositionScreenCenter.normalized * m_offset * m_offsetMagnitude; 
-        transform.position = m_cameraPosition;
+        transform.position =
+            Vector3.SmoothDamp(
+                transform.position,
+                m_cameraPosition,
+                ref velocity,
+                m_smoothDampSpeed,
+                m_smoothDampMaxSpeed
+            );
     }
 
+    public float MagnitudeFromAngle(float angle)
+    {
+        float absCosAngle = Mathf.Abs(Mathf.Cos(angle));
+        float absSinAngle = Mathf.Abs(Mathf.Sin(angle));
+
+        float magnitude = 0f;
+
+        if (m_camera.pixelWidth / 2f * absSinAngle <= m_camera.pixelHeight / 2f * absCosAngle)
+            magnitude = m_camera.pixelWidth / 2f / absCosAngle;
+        else
+            magnitude = m_camera.pixelHeight / 2f / absSinAngle;
+
+        return magnitude;
+    }
 
     #endregion
 }
