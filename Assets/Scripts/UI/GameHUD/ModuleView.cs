410cd18de9a219c6de94435198990676bd5a63bd
Pierre Mizzi <pierre.mizza@gmail.com>
2023-08-03 16:04:47 +0900

    Production progress feedback

--- Assets/Scripts/UI/GameHUD/ModuleView.cs
+++ Assets/Scripts/UI/GameHUD/ModuleView.cs
@@ -2,46 +2,24 @@ using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-// ModuleManager -> Module -> ModuleHUD -> Module(VisualElement)
-// ModuleHUD
-
 public class ModuleView
 {
-    /*
-        - Switch state Active / Inactive                    DONE
-        - Crystal Energy remaining/Capacity                 DONE
-        - Stored Energy remaining/Capacity                  DONE
-        - Switch mode FIRE / EXTRACT
-            - Change Label                                  DONE
-            - FIRE MODE
-                - Change border on energy type
-                - Hide arrow                                DONE
-                - Display "Fire" tip
-            - EXTRACT MODE
-                - Hide border on energy type
-                - Display arrow                             DONE
-                - Hide "Fire" tip
-    */
-
-    // TODO : Progress Bar extraction
 
     #region Fields
 
     #region Behaviour
 
-
     private VisualElement m_root;
-
-    private Module m_module;
+    private Module m_turret;
 
     #endregion
 
-    #region Activable
+    #region Droppable
 
-    private const string k_activableLabel = "module-hud-inactive__label";
-    private Label m_activableLabel;
-    private const string k_isNotActivableText = "Search asteroid";
-    private const string k_isActivableText = "<b>Press E</b> to drop module";
+    private const string k_droppableLabel = "module-hud-inactive__label";
+    private Label m_droppableLabel;
+    private const string k_isNotDroppableText = "Search asteroid";
+    private const string k_isDroppableText = "<b>Right click</b> to drop module";
 
     #endregion
 
@@ -55,22 +33,23 @@ public class ModuleView
 
     #endregion
 
-
     #region Mode
 
     private const string k_modeLabel = "module-hud__mode-label";
-    private const string k_extractionArrow = "resources-arrow";
     private const string k_crystalEnergyContainer = "resources-crystal-container";
     private const string k_storedEnergyContainer = "resources-stored-container";
     private const string k_crystalEnergyLabel = "resources-crystal-label";
     private const string k_storedEnergyLabel = "resources-stored-label";
+    private const string k_productionProgressContainer = "production-progress-container";
+    private const string k_productionProgressFill = "production-progress-fill";
 
     private Label m_modelLabel;
-    private VisualElement m_extractionArrow;
     private VisualElement m_crystalEnergyContainer;
     private VisualElement m_storedEnergyContainer;
     private Label m_crystalEnergyLabel;
     private Label m_storedEnergyLabel;
+    private VisualElement m_productionProgressContainer;
+    private VisualElement m_productionProgressFill;
 
     private const string k_availableResourceClass = "available-resources";
 
@@ -99,7 +78,7 @@ public class ModuleView
 
     public void Initialize(Module module, VisualElement root)
     {
-        m_module = module;
+        m_turret = module;
 
         // Instantiate Visual Asset
         m_root = root;
@@ -114,20 +93,22 @@ public class ModuleView
     private void InitializeVisualElements()
     {
         // Activable
-        m_activableLabel = m_root.Q<Label>(k_activableLabel);
+        m_droppableLabel = m_root.Q<Label>(k_droppableLabel);
 
         // Status
         m_activeContainer = m_root.Q(k_activeContainer);
         m_inactiveContainer = m_root.Q(k_inactiveContainer);
 
         m_modelLabel = m_root.Q<Label>(k_modeLabel);
-        m_extractionArrow = m_root.Q(k_extractionArrow);
+        m_productionProgressContainer = m_root.Q(k_productionProgressContainer);
         m_crystalEnergyContainer = m_root.Q(k_crystalEnergyContainer);
         m_storedEnergyContainer = m_root.Q(k_storedEnergyContainer);
 
         // Energy
         m_crystalEnergyLabel = m_root.Q<Label>(k_crystalEnergyLabel);
         m_storedEnergyLabel = m_root.Q<Label>(k_storedEnergyLabel);
+
+        m_productionProgressFill = m_root.Q(k_productionProgressFill);
 
         // Tips
         m_retrieveTip = m_root.Q(k_retrieveTip);
@@ -144,32 +125,32 @@ public class ModuleView
     private void SubscribeToModel()
     {
         // Activable
-        m_module.onIsDroppable += CallbackIsDroppable;
+        m_turret.onIsDroppable += CallbackIsDroppable;
 
         // Targeted
-        m_module.onIsTargeted += CallbackIsTargeted;
+        m_turret.onIsTargeted += CallbackIsTargeted;
 
         // Status
-        m_module.onChangeState += CallbackChangeState;
+        m_turret.onChangeState += CallbackChangeState;
 
         // Energy
-        m_module.onAssignCrystal += CallbackAssignCrystal;
-        m_module.onRemoveCrystal += CallbackRemoveCrystal;
+        m_turret.onAssignCrystal += CallbackAssignCrystal;
+        m_turret.onRemoveCrystal += CallbackRemoveCrystal;
 
-        m_module.onRefreshEnergy += CallbackRefreshEnergy;
-        m_module.onProductionProgress += CallbackProductionProgress;
+        m_turret.onRefreshEnergy += CallbackRefreshEnergy;
+        m_turret.onProductionProgress += CallbackProductionProgress;
     }
 
 
 
     private void CallbackAssignCrystal()
     {
-        m_module.crystal.onRefreshEnergy += CallbackRefreshEnergy;
+        m_turret.crystal.onRefreshEnergy += CallbackRefreshEnergy;
     }
 
     private void CallbackRemoveCrystal()
     {
-        m_module.crystal.onRefreshEnergy -= CallbackRefreshEnergy;
+        m_turret.crystal.onRefreshEnergy -= CallbackRefreshEnergy;
     }
 
     private void UnsubscribeToModel() { }
@@ -180,7 +161,7 @@ public class ModuleView
 
     private void CallbackIsDroppable()
     {
-        m_activableLabel.text = (m_module.isDroppable) ? k_isActivableText : k_isNotActivableText;
+        m_droppableLabel.text = m_turret.isDroppable ? k_isDroppableText : k_isNotDroppableText;
     }
 
     #endregion
@@ -189,11 +170,11 @@ public class ModuleView
 
     private void CallbackIsTargeted()
     {
-        m_retrieveTip.style.display = m_module.isTargeted ? DisplayStyle.Flex : DisplayStyle.None;
+        m_retrieveTip.style.display = m_turret.isTargeted ? DisplayStyle.Flex : DisplayStyle.None;
 
-        TurretStateType state = (TurretStateType)m_module.currentState.type;
+        TurretStateType state = (TurretStateType)m_turret.currentState.type;
 
-        if (m_module.isTargeted)
+        if (m_turret.isTargeted)
         {
             m_productionModeTip.style.display = state == TurretStateType.Offensive ? DisplayStyle.Flex : DisplayStyle.None;
             m_offensiveModeTip.style.display = state == TurretStateType.Production ? DisplayStyle.Flex : DisplayStyle.None;
@@ -239,7 +220,7 @@ public class ModuleView
         m_inactiveContainer.style.display = DisplayStyle.None;
 
         m_modelLabel.text = "OFFENSIVE MODE";
-        m_extractionArrow.style.visibility = Visibility.Hidden;
+        m_productionProgressContainer.style.visibility = Visibility.Hidden;
         m_fireTip.style.display = DisplayStyle.Flex;
     }
 
@@ -249,7 +230,7 @@ public class ModuleView
         m_inactiveContainer.style.display = DisplayStyle.None;
 
         m_modelLabel.text = "PRODUCTION MODE";
-        m_extractionArrow.style.visibility = Visibility.Visible;
+        m_productionProgressContainer.style.visibility = Visibility.Visible;
         m_fireTip.style.display = DisplayStyle.None;
     }
 
@@ -268,7 +249,7 @@ public class ModuleView
     private void CallbackRefreshEnergy()
     {
         // Manages the priority resource
-        if (m_module.storedEnergy > 0)
+        if (m_turret.storedEnergy > 0)
         {
             m_crystalEnergyContainer.RemoveFromClassList(k_availableResourceClass);
             m_storedEnergyContainer.AddToClassList(k_availableResourceClass);
@@ -279,18 +260,18 @@ public class ModuleView
             m_storedEnergyContainer.RemoveFromClassList(k_availableResourceClass);
         }
 
-        m_crystalEnergyLabel.text = m_module.crystal.remainingEnergyCount.ToString();
+        m_crystalEnergyLabel.text = m_turret.crystal.remainingEnergyCount.ToString();
 
         m_storedEnergyLabel.text = string.Format(
             "{0} / {1}",
-            m_module.storedEnergy,
-            m_module.settings.maxStoredEnergy
+            m_turret.storedEnergy,
+            m_turret.settings.maxStoredEnergy
         );
     }
 
     private void CallbackProductionProgress(float value)
     {
-
+        m_productionProgressFill.style.width = Length.Percent(value * 100f);
     }
 
     #endregion
