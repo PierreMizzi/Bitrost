a3d9403eb81ff0214ac1100b1ecb65d2f978afd6
Pierre Mizzi <pierre.mizza@gmail.com>
2023-08-05 07:14:51 +0900

    Integration of Unity for Visual Studio Code Package

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,179 +5,179 @@ using UnityEngine.InputSystem;
 [ExecuteInEditMode]
 public class CameraController : MonoBehaviour, IPausable
 {
-    #region Fields
+	#region Fields
 
-    [SerializeField]
-    private LevelChannel m_levelChannel;
+	[SerializeField]
+	private LevelChannel m_levelChannel;
 
-    [SerializeField]
-    private Transform m_target = null;
+	[SerializeField]
+	private Transform m_target = null;
 
-    private Vector3 m_targetPosition;
+	private Vector3 m_targetPosition;
 
-    private Camera m_camera = null;
+	private Camera m_camera = null;
 
-    #region Mouse Position 
+	#region Mouse Position 
 
-    [Header("Inputs")]
-    [SerializeField]
-    private InputActionReference m_mousePositionActionReference = null;
+	[Header("Inputs")]
+	[SerializeField]
+	private InputActionReference m_mousePositionActionReference = null;
 
-    private Vector3 m_mousePositionValue;
+	private Vector3 m_mousePositionValue;
 
-    private Vector3 mousePositionScreenCenter
-    {
-        get
-        {
-            return new Vector3(
-                m_mousePositionValue.x - m_camera.pixelWidth / 2f,
-                m_mousePositionValue.y - m_camera.pixelHeight / 2f
-            );
-        }
-    }
+	private Vector3 mousePositionScreenCenter
+	{
+		get
+		{
+			return new Vector3(
+				m_mousePositionValue.x - m_camera.pixelWidth / 2f,
+				m_mousePositionValue.y - m_camera.pixelHeight / 2f
+			);
+		}
+	}
 
-    #endregion
+	#endregion
 
-    #region Screen Edge panning
+	#region Screen Edge panning
 
-    [SerializeField]
-    private float m_treshold = 0.75f;
+	[SerializeField]
+	private float m_treshold = 0.75f;
 
-    [SerializeField]
-    private float m_offsetMagnitude = 1f;
+	[SerializeField]
+	private float m_offsetMagnitude = 1f;
 
-    [SerializeField]
-    private float m_smoothDampSpeed;
+	[SerializeField]
+	private float m_smoothDampSpeed;
 
-    [SerializeField]
-    private float m_smoothDampMaxSpeed;
+	[SerializeField]
+	private float m_smoothDampMaxSpeed;
 
-    private float m_offset;
+	private float m_offset;
 
-    private Vector3 velocity;
+	private Vector3 velocity;
 
-    private Vector3 m_cameraOffsetPosition;
-    private Vector3 m_previousCameraOffsetPosition;
+	private Vector3 m_cameraOffsetPosition;
+	private Vector3 m_previousCameraOffsetPosition;
 
-    #endregion
+	#endregion
 
-    public bool isPaused { get; set; }
+	public bool isPaused { get; set; }
 
-    #endregion
+	#endregion
 
-    #region Methods
+	#region Methods
 
-    #region MonoBehaviour
+	#region MonoBehaviour
 
-    private void Awake()
-    {
-        m_camera = Camera.main;
-    }
+	private void Awake()
+	{
+		m_camera = Camera.main;
+	}
 
-    private void Start()
-    {
-        if (m_levelChannel != null)
-        {
-            m_levelChannel.onPauseGame += Pause;
-            m_levelChannel.onResumeGame += Resume;
+	private void Start()
+	{
+		if (m_levelChannel != null)
+		{
+			m_levelChannel.onPauseGame += Pause;
+			m_levelChannel.onResumeGame += Resume;
 
-        }
-    }
+		}
+	}
 
-    private void Update()
-    {
-        if (!isPaused)
-            m_mousePositionValue = m_mousePositionActionReference.action.ReadValue<Vector2>();
-    }
+	private void Update()
+	{
+		if (!isPaused)
+			m_mousePositionValue = m_mousePositionActionReference.action.ReadValue<Vector2>();
+	}
 
-    private void LateUpdate()
-    {
-        if (!isPaused)
-        {
-            m_targetPosition = m_target.position;
-            m_targetPosition.z = -1;
+	private void LateUpdate()
+	{
+		if (!isPaused)
+		{
+			m_targetPosition = m_target.position;
+			m_targetPosition.z = -1;
 
-            ManageCameraOffset();
-            transform.position = m_targetPosition + m_cameraOffsetPosition;
-        }
-    }
+			ManageCameraOffset();
+			transform.position = m_targetPosition + m_cameraOffsetPosition;
+		}
+	}
 
-    private void OnDestroy()
-    {
-        if (m_levelChannel != null)
-        {
-            m_levelChannel.onPauseGame -= Pause;
-            m_levelChannel.onResumeGame -= Resume;
-        }
-    }
+	private void OnDestroy()
+	{
+		if (m_levelChannel != null)
+		{
+			m_levelChannel.onPauseGame -= Pause;
+			m_levelChannel.onResumeGame -= Resume;
+		}
+	}
 
-    #endregion
+	#endregion
 
-    #region Screen Edge panning
+	#region Screen Edge panning
 
-    private void ManageCameraOffset()
-    {
-        float angle = Mathf.Atan2(mousePositionScreenCenter.y, mousePositionScreenCenter.x);
+	private void ManageCameraOffset()
+	{
+		float angle = Mathf.Atan2(mousePositionScreenCenter.y, mousePositionScreenCenter.x);
 
-        float edgeMagnitude = MagnitudeFromAngle(angle);
+		float edgeMagnitude = MagnitudeFromAngle(angle);
 
-        float thresholdEdgeMagnitude = edgeMagnitude * m_treshold;
+		float thresholdEdgeMagnitude = edgeMagnitude * m_treshold;
 
-        float mousePositionMangitude = mousePositionScreenCenter.magnitude;
+		float mousePositionMangitude = mousePositionScreenCenter.magnitude;
 
-        m_offset = UtilsClass.Remap(
-            mousePositionMangitude,
-            thresholdEdgeMagnitude,
-            edgeMagnitude,
-            0f,
-            1f
-        );
-        m_offset = Mathf.Clamp01(m_offset);
+		m_offset = UtilsClass.Remap(
+			mousePositionMangitude,
+			thresholdEdgeMagnitude,
+			edgeMagnitude,
+			0f,
+			1f
+		);
+		m_offset = Mathf.Clamp01(m_offset);
 
-        m_cameraOffsetPosition =
-            mousePositionScreenCenter.normalized * m_offset * m_offsetMagnitude;
+		m_cameraOffsetPosition =
+			mousePositionScreenCenter.normalized * m_offset * m_offsetMagnitude;
 
-        m_cameraOffsetPosition = Vector3.SmoothDamp(
-            m_previousCameraOffsetPosition,
-            m_cameraOffsetPosition,
-            ref velocity,
-            m_smoothDampSpeed,
-            m_smoothDampMaxSpeed
-        );
-        m_previousCameraOffsetPosition = m_cameraOffsetPosition;
-    }
+		m_cameraOffsetPosition = Vector3.SmoothDamp(
+			m_previousCameraOffsetPosition,
+			m_cameraOffsetPosition,
+			ref velocity,
+			m_smoothDampSpeed,
+			m_smoothDampMaxSpeed
+		);
+		m_previousCameraOffsetPosition = m_cameraOffsetPosition;
+	}
 
-    public float MagnitudeFromAngle(float angle)
-    {
-        float absCosAngle = Mathf.Abs(Mathf.Cos(angle));
-        float absSinAngle = Mathf.Abs(Mathf.Sin(angle));
+	public float MagnitudeFromAngle(float angle)
+	{
+		float absCosAngle = Mathf.Abs(Mathf.Cos(angle));
+		float absSinAngle = Mathf.Abs(Mathf.Sin(angle));
 
-        float magnitude = 0f;
+		float magnitude = 0f;
 
-        if (m_camera.pixelWidth / 2f * absSinAngle <= m_camera.pixelHeight / 2f * absCosAngle)
-            magnitude = m_camera.pixelWidth / 2f / absCosAngle;
-        else
-            magnitude = m_camera.pixelHeight / 2f / absSinAngle;
+		if (m_camera.pixelWidth / 2f * absSinAngle <= m_camera.pixelHeight / 2f * absCosAngle)
+			magnitude = m_camera.pixelWidth / 2f / absCosAngle;
+		else
+			magnitude = m_camera.pixelHeight / 2f / absSinAngle;
 
-        return magnitude;
-    }
+		return magnitude;
+	}
 
-    #endregion
+	#endregion
 
-    #region Pause
+	#region Pause
 
 
-    public void Pause()
-    {
-        isPaused = true;
-    }
+	public void Pause()
+	{
+		isPaused = true;
+	}
 
-    public void Resume()
-    {
-        isPaused = false;
-    }
+	public void Resume()
+	{
+		isPaused = false;
+	}
 
-    #endregion
+	#endregion
 
-    #endregion
+	#endregion
 }
