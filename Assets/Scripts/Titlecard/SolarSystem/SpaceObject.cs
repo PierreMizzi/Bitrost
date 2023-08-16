d29567d246c26f5851dc66832e56fa8864cbffec
Pierre Mizzi <pierre.mizza@gmail.com>
2023-08-16 10:38:19 +0900

    OrbitTrail + fixed Update

--- Assets/Scripts/Titlecard/SolarSystem/SpaceObject.cs
+++ Assets/Scripts/Titlecard/SolarSystem/SpaceObject.cs
@@ -1,5 +1,6 @@
 namespace Bitfrost.Application.SolarSystem
 {
+	using System.Collections.Generic;
 	using PierreMizzi.Rendering;
 	using PierreMizzi.Useful;
 	using UnityEngine;
@@ -23,24 +24,26 @@ namespace Bitfrost.Application.SolarSystem
 
 		private Vector3 m_orbitPosition;
 
-		private float m_rotationAngle;
+		private Quaternion m_frameRotation;
 
 		[Header("Rendering")]
 
 		[SerializeField]
 		private SpriteRenderer m_objectSprite;
 
-		[SerializeField]
-		private ParticleSystem m_trail;
-
 		[Header("Orbit")]
-
 		[SerializeField]
 		private SpriteRenderer m_orbitSprite;
 
 		private MaterialPropertyBlockModifier m_orbitPropertyBlock;
 
 		private const string k_radiusProperty = "_Radius";
+
+		[Header("System")]
+		[SerializeField]
+		private bool m_isSystemRoot;
+
+		[SerializeField] private List<SpaceObject> m_children = new List<SpaceObject>();
 
 		private float orbitRadiusShader
 		{
@@ -67,6 +70,9 @@ namespace Bitfrost.Application.SolarSystem
 
 		private void OnValidate()
 		{
+			if (m_orbitCenter == null)
+				return;
+
 			m_orbitSprite.transform.position = m_orbitCenter.position;
 			m_orbitPosition = new Vector2(m_orbitRadius, 0);
 			transform.position = m_orbitCenter.position + m_orbitPosition;
@@ -77,17 +83,8 @@ namespace Bitfrost.Application.SolarSystem
 
 		protected void Update()
 		{
-			if (Application.isPlaying)
-				UpdateRotation();
-		}
-
-		protected void LateUpdate()
-		{
-			if (Application.isPlaying)
-			{
-				m_orbitSprite.transform.position = m_orbitCenter.position;
-				transform.position = m_orbitCenter.position + m_orbitPosition;
-			}
+			if (Application.isPlaying && m_isSystemRoot)
+				UpdateOrbit();
 		}
 
 		protected void InitializeRotation()
@@ -95,11 +92,21 @@ namespace Bitfrost.Application.SolarSystem
 			Quaternion randomRotation = UtilsClass.RandomRotation2D();
 			m_orbitPosition = new Vector2(m_orbitRadius, 0);
 			m_orbitPosition = randomRotation * m_orbitPosition;
+
+			m_orbitSprite.transform.rotation = randomRotation;
 		}
 
-		protected void UpdateRotation()
+		protected void UpdateOrbit()
 		{
-			m_orbitPosition = Quaternion.Euler(0f, 0f, Time.deltaTime * m_speed) * m_orbitPosition;
+			m_frameRotation = Quaternion.Euler(0f, 0f, Time.deltaTime * m_speed);
+			m_orbitPosition = m_frameRotation * m_orbitPosition;
+
+			m_orbitSprite.transform.position = m_orbitCenter.position;
+			m_orbitSprite.transform.rotation *= m_frameRotation;
+			transform.position = m_orbitCenter.position + m_orbitPosition;
+
+			foreach (SpaceObject child in m_children)
+				child.UpdateOrbit();
 		}
 
 
